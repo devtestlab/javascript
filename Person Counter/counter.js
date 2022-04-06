1281e3d8e40da9fdab043d89ea19f1a97695abba
Gourav Kumar <>
2022-04-06 12:03:06 +0530

    JavaScript- Person counter excercise

---
+++ Person Counter/counter.js
@@ -0,0 +1,42 @@
+// Function Call
+// let lapCompleted = 0
+// function lapCount () {
+//     lapCompleted = lapCompleted + 1
+// }
+// lapCount()
+// lapCount()
+// lapCount()
+// lapCount()
+// console.log(lapCompleted)
+
+// Addition
+// let firstBatch = 5
+// let secondBatch = 20
+// let count = firstBatch + secondBatch
+// console.log(count)
+
+// Multiplication
+// let myAge = 30
+// let humanDogRatio = 15
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
+
+// Operation
+// let bonusPoints = 50
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+// String Processing
+let username = "Gourav Kumar"
+let message = "You have new notifications in inbox"
+let messageToUser = message + ", " + username + "!"
+console.log(messageToUser)
+
+let name = "Gourav Kashyap"
+let greeting = "Hi, Good Morning How you doing "
+let myGreeting =  greeting + name + "!"
+console.log(myGreeting)
